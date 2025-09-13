3c035215b909836f1596328fc828a5af0779ce01
Devuh <david.walter.reichert@gmail.com>
2025-09-13 18:20:37 -0500

    Create player grids using Javascript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./styles.css";
 
-console.log("JS Webpack Template");
+function drawGrid(grid) {
+  for (let i = 0; i < 10; i++) {
+    const column = document.createElement('div');
+    column.setAttribute('class','column');
+    grid.appendChild(column);
+    for(let i = 0; i < 10; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('class','square');
+      column.appendChild(square);
+    }
+  }
+}
+
+const grids = document.querySelectorAll('.grid');
+drawGrid(grids[0]);
+drawGrid(grids[1]);
