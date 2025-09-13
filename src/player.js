a37f942a163ff2297f7adbfc5e2be4918e2f961c
Devuh <david.walter.reichert@gmail.com>
2025-09-13 16:23:11 -0500

    Create new Player class which initializes a new board and allows for human or computer type

---
+++ src/player.js
@@ -0,0 +1,10 @@
+import Gameboard from './gameboard.js';
+
+export default class Player {
+  constructor(type = 'human') {
+    type = type.toLowerCase();
+    if(type !== 'human' && type !== 'computer') throw new Error('Invalid player type');
+    this.type = type;
+    this.board = new Gameboard();
+  }
+}
