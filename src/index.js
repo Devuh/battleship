69af5782c3326f3344d512b4fb75c67eb27cbacc
Devuh <david.walter.reichert@gmail.com>
2025-09-13 18:42:54 -0500

    Wrap drawGrid in DOM class

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import "./styles.css";
 
-function drawGrid(grid) {
-  for (let i = 0; i < 10; i++) {
-    const column = document.createElement('div');
-    column.setAttribute('class','column');
-    grid.appendChild(column);
-    for(let i = 0; i < 10; i++) {
-      const square = document.createElement('div');
-      square.setAttribute('class','square');
-      column.appendChild(square);
+class DOM {
+  static drawGrid(grid) {
+    for (let i = 0; i < 10; i++) {
+      const column = document.createElement('div');
+      column.setAttribute('class','column');
+      grid.appendChild(column);
+      for(let i = 0; i < 10; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class','square');
+        column.appendChild(square);
+      }
     }
   }
 }
 
-const grids = document.querySelectorAll('.grid');
-drawGrid(grids[0]);
-drawGrid(grids[1]);
+let grids = document.querySelectorAll('.grid');
+
+DOM.drawGrid(grids[0]);
+DOM.drawGrid(grids[1]);
