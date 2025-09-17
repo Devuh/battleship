33ffad8c96f48a5ced9cdd1c7162c791aced4378
Devuh <david.walter.reichert@gmail.com>
2025-09-17 02:18:38 -0500

    Update tests to reflect method name change of placeShip to createShip

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -6,7 +6,7 @@ describe("Gameboard", () => {
     it("Place ship of length 3, direction right at (3,6)", () => {
       let grid = new Gameboard();
       let ship = new Ship(3);
-      grid.placeShip(3, 6, "right", ship);
+      grid.createShip(3, 6, "right", ship);
 
       expect(grid.board).toStrictEqual([
         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -25,7 +25,7 @@ describe("Gameboard", () => {
     it("Place ship of length 2, direction down at (0,4)", () => {
       let grid = new Gameboard();
       let ship = new Ship(2);
-      grid.placeShip(0, 4, "down", ship);
+      grid.createShip(0, 4, "down", ship);
 
       expect(grid.board).toStrictEqual([
         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -44,25 +44,25 @@ describe("Gameboard", () => {
     it("Place ship of length 2, direction down at (10,4)", () => {
       let grid = new Gameboard();
       let ship = new Ship(2);
-      expect(() => grid.placeShip(10, 4, "down", ship)).toThrow(Error);
+      expect(() => grid.createShip(10, 4, "down", ship)).toThrow(Error);
     });
 
     it("Place ship of length 2, direction down at (-1,4)", () => {
       let grid = new Gameboard();
       let ship = new Ship(2);
-      expect(() => grid.placeShip(-1, 4, "down", ship)).toThrow(Error);
+      expect(() => grid.createShip(-1, 4, "down", ship)).toThrow(Error);
     });
 
     it("Place ship of length 4, direction right at (9,4)", () => {
       let grid = new Gameboard();
       let ship = new Ship(4);
-      expect(() => grid.placeShip(9, 4, "right", ship)).toThrow(Error);
+      expect(() => grid.createShip(9, 4, "right", ship)).toThrow(Error);
     });
 
     it("Place ship of length 2, direction down at (9,4)", () => {
       let grid = new Gameboard();
       let ship = new Ship(2);
-      grid.placeShip(9, 4, "down", ship);
+      grid.createShip(9, 4, "down", ship);
       expect(grid.board).toStrictEqual([
         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -80,13 +80,13 @@ describe("Gameboard", () => {
     it("Place ship of length 4, direction down at (9,9)", () => {
       let grid = new Gameboard();
       let ship = new Ship(4);
-      expect(() => grid.placeShip(9, 9, "down", ship)).toThrow(Error);
+      expect(() => grid.createShip(9, 9, "down", ship)).toThrow(Error);
     });
 
     it("Place ship of length 2, direction right at (8,8)", () => {
       let grid = new Gameboard();
       let ship = new Ship(2);
-      grid.placeShip(8, 8, "right", ship);
+      grid.createShip(8, 8, "right", ship);
       expect(grid.board).toStrictEqual([
         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "],
@@ -105,8 +105,8 @@ describe("Gameboard", () => {
       let grid = new Gameboard();
       let ship1 = new Ship(3);
       let ship2 = new Ship(3);
-      grid.placeShip(5, 4, "down", ship1);
-      expect(() => grid.placeShip(4, 5, "right", ship2)).toThrow(Error);
+      grid.createShip(5, 4, "down", ship1);
+      expect(() => grid.createShip(4, 5, "right", ship2)).toThrow(Error);
     });
   });
 
@@ -137,7 +137,7 @@ describe("Gameboard", () => {
     it("Receive attack on ocuppied space (3,5)", () => {
       let grid = new Gameboard();
       let ship = new Ship(3);
-      grid.placeShip(3, 5, "right", ship);
+      grid.createShip(3, 5, "right", ship);
       grid.receiveAttack(3, 5);
       expect(ship.hits).toBe(1);
       expect(grid.board).toStrictEqual([
@@ -157,7 +157,7 @@ describe("Gameboard", () => {
     it("Receive attack on ocuppied space (3,5) twice", () => {
       let grid = new Gameboard();
       let ship = new Ship(3);
-      grid.placeShip(3, 5, "right", ship);
+      grid.createShip(3, 5, "right", ship);
       grid.receiveAttack(3, 5);
       expect(ship.hits).toBe(1);
       expect(grid.board).toStrictEqual([
@@ -178,7 +178,7 @@ describe("Gameboard", () => {
     it("Receive 3 attacks on ship of length 3", () => {
       let grid = new Gameboard();
       let ship = new Ship(3);
-      grid.placeShip(3, 5, "right", ship);
+      grid.createShip(3, 5, "right", ship);
       expect(grid.isAllShipsSunk()).toBeFalsy();
       grid.receiveAttack(3, 5);
       grid.receiveAttack(4, 5);
@@ -203,7 +203,7 @@ describe("Gameboard", () => {
     it("Receive 2 attacks on empty spaces then 2 on occupied spaces", () => {
       let grid = new Gameboard();
       let ship = new Ship(3);
-      grid.placeShip(3, 5, "right", ship);
+      grid.createShip(3, 5, "right", ship);
       grid.receiveAttack(0, 0);
       grid.receiveAttack(4, 3);
       grid.receiveAttack(3, 5);
