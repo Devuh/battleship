29ea7a1550554d6e82aeaabceb926c3dd93684bd
Devuh <david.walter.reichert@gmail.com>
2025-09-17 02:19:12 -0500

    Create placeHit() method with accompanying hit and miss style classes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,24 @@ import Gameboard from "./gameboard.js";
 class DOM {
   static domGrids = document.querySelectorAll('.grid');
 
+  static #getSquare(x,y,player) {
+    return document.getElementById(`${x}-${y}-${GameController.players.indexOf(player)+1}`);
+  }
+
+  static placeHit(x,y,player) {
+    let square = this.#getSquare(x,y,player);
+    player.board.receiveAttack(x,y);
+    if(player.board.board[y][x] === 'o') {
+      square.classList.add('miss');
+    } else {
+      square.classList.add('hit');
+    }
+  }
+
   static placeShip(x,y,direction,ship,player) {
     player.board.createShip(x,y,direction,ship);
     for (let i = 0; i < ship.length; i++) {
-      let square = document.getElementById(`${x}-${y}-${GameController.players.indexOf(player)+1}`);
+      let square = this.#getSquare(x,y,player);
       square.classList.add('ship');
       direction === "right" ? x++ : y++;
     }
@@ -37,8 +51,8 @@ DOM.drawGrids();
 
 // Populate grid with random coordinates
 
-let player2 = GameController.player2;
 let player1 = GameController.player1;
+let player2 = GameController.player2;
 
 let player1Ship1 = new Ship(3);
 let player1Ship2 = new Ship(4);
@@ -55,6 +69,9 @@ DOM.placeShip(9,6,'down',player1Ship3,player1);
 DOM.placeShip(0,0,'down',player2Ship1,player2);
 DOM.placeShip(3,4,'right',player2Ship2,player2);
 DOM.placeShip(6,9,'right',player2Ship3,player2);
+
+DOM.placeHit(4,4,player2);
+DOM.placeHit(2,4,player2);
 
 console.log(player1.board);
 console.log(player2.board);
